f1ae9fd80e14b7b261ea34929cde78bbd95c3e1f
Leonid Sokolinsky <Leonid.Sokolinsky@susu.ru>
2024-06-23 15:31:38 +0500

    Add files via upload
    Function PC_bsf_MapInit() has been renamed to PC_bsf_IterInit().
    On the master, PC_bsf_IterInit() is now executed at the beginning of each iteration.

--- Problem-bsfCode.cpp
+++ Problem-bsfCode.cpp
@@ -13,22 +13,52 @@ This source code is a part of BSF Skeleton (https://github.com/leonid-sokolinsky
 using namespace std;
 
 //----------------------- Predefined problem-dependent functions -----------------
-void PC_bsf_SetInitParameter(PT_bsf_parameter_T* parameter) {
+void PC_bsf_CopyParameter(PT_bsf_parameter_T parameterIn, PT_bsf_parameter_T* parameterOutP) {
+
 }
 
 void PC_bsf_Init(bool* success) {
 
 }
 
-void PC_bsf_SetListSize(int* listSize) {
-
-}
-
-void PC_bsf_SetMapListElem(PT_bsf_mapElem_T* elem, int i) {
-
-}
 
 void PC_bsf_IterInit(PT_bsf_parameter_T parameter) {
+	// Optional filling. Do not delete!
+}
+
+void PC_bsf_IterOutput(PT_bsf_reduceElem_T* reduceResult, int reduceCounter, PT_bsf_parameter_T parameter,
+	double elapsedTime, int nextJob) {	// For Job 0
+	cout << "------------------ " << BSF_sv_iterCounter << " ------------------" << endl;
+
+}
+
+void PC_bsf_IterOutput_1(PT_bsf_reduceElem_T_1* reduceResult, int reduceCounter, PT_bsf_parameter_T parameter,
+	double elapsedTime, int nextJob) {	// For Job 1
+	cout << "------------------ " << BSF_sv_iterCounter << " ------------------" << endl;
+	// Optional filling. Do not delete!
+
+}
+
+void PC_bsf_IterOutput_2(PT_bsf_reduceElem_T_2* reduceResult, int reduceCounter, PT_bsf_parameter_T parameter,
+	double elapsedTime, int nextJob) {	// For Job 2
+	cout << "------------------ " << BSF_sv_iterCounter << " ------------------" << endl;
+	// Optional filling. Do not delete!
+
+}
+
+void PC_bsf_IterOutput_3(PT_bsf_reduceElem_T_3* reduceResult, int reduceCounter, PT_bsf_parameter_T parameter,
+	double elapsedTime, int nextJob) {	// For Job 3
+	cout << "------------------ " << BSF_sv_iterCounter << " ------------------" << endl;
+	// Optional filling. Do not delete!
+
+}
+
+void PC_bsf_JobDispatcher(
+	PT_bsf_parameter_T* parameter, // Current Approximation
+	int* job,
+	bool* exit,
+	double t
+) {
 	// Optional filling. Do not delete!
 }
 
@@ -48,20 +78,39 @@ void PC_bsf_MapF_3(PT_bsf_mapElem_T* mapElem, PT_bsf_reduceElem_T_3* reduceElem,
 	// Optional filling. Do not delete!
 }
 
-void PC_bsf_ReduceF(PT_bsf_reduceElem_T* x, PT_bsf_reduceElem_T* y, PT_bsf_reduceElem_T* z) {			// For Job 0
-	// z = x (+) y
+void PC_bsf_ParametersOutput(PT_bsf_parameter_T parameter) {
+	cout << "=================================================== Problem parameters ====================================================" << endl;
+	cout << "Number of Workers: " << BSF_sv_numOfWorkers << endl;
+#ifdef PP_BSF_OMP
+#ifdef PP_BSF_NUM_THREADS
+	cout << "Number of Threads: " << PP_BSF_NUM_THREADS << endl;
+#else
+	cout << "Number of Threads: " << omp_get_num_procs() << endl;
+#endif // PP_BSF_NUM_THREADS
+#else
+	cout << "OpenMP is turned off!" << endl;
+#endif // PP_BSF_OMP
+
 }
 
-void PC_bsf_ReduceF_1(PT_bsf_reduceElem_T_1* x, PT_bsf_reduceElem_T_1* y, PT_bsf_reduceElem_T_1* z) {	// For Job 1
+void PC_bsf_ProblemOutput(PT_bsf_reduceElem_T* reduceResult, int reduceCounter, PT_bsf_parameter_T parameter,
+	double t) {	// For Job 0
+
+}
+
+void PC_bsf_ProblemOutput_1(PT_bsf_reduceElem_T_1* reduceResult, int reduceCounter, PT_bsf_parameter_T parameter,
+	double t) {	// For Job 1
 	// Optional filling. Do not delete!
 }
 
-void PC_bsf_ReduceF_2(PT_bsf_reduceElem_T_2* x, PT_bsf_reduceElem_T_2* y, PT_bsf_reduceElem_T_2* z) {	// For Job 2
+void PC_bsf_ProblemOutput_2(PT_bsf_reduceElem_T_2* reduceResult, int reduceCounter, PT_bsf_parameter_T parameter,
+	double t) {	// For Job 2
 	// Optional filling. Do not delete!
 }
 
-void PC_bsf_ReduceF_3(PT_bsf_reduceElem_T_3* x, PT_bsf_reduceElem_T_3* y, PT_bsf_reduceElem_T_3* z) {	// For Job 3
-	// optional filling
+void PC_bsf_ProblemOutput_3(PT_bsf_reduceElem_T_3* reduceResult, int reduceCounter, PT_bsf_parameter_T parameter,
+	double t) {	// For Job 3
+	// Optional filling. Do not delete!
 }
 
 void PC_bsf_ProcessResults(		// For Job 0
@@ -104,79 +153,32 @@ void PC_bsf_ProcessResults_3(	// For Job 3
 	// Optional filling. Do not delete!
 }
 
-void PC_bsf_JobDispatcher(
-	PT_bsf_parameter_T* parameter, // Current Approximation
-	int* job,
-	bool* exit,
-	double t
-) {
+void PC_bsf_ReduceF(PT_bsf_reduceElem_T* x, PT_bsf_reduceElem_T* y, PT_bsf_reduceElem_T* z) {			// For Job 0
+	// z = x (+) y
+}
+
+void PC_bsf_ReduceF_1(PT_bsf_reduceElem_T_1* x, PT_bsf_reduceElem_T_1* y, PT_bsf_reduceElem_T_1* z) {	// For Job 1
 	// Optional filling. Do not delete!
 }
 
-void PC_bsf_ParametersOutput(PT_bsf_parameter_T parameter) {
-	cout << "=================================================== Problem parameters ====================================================" << endl;
-	cout << "Number of Workers: " << BSF_sv_numOfWorkers << endl;
-#ifdef PP_BSF_OMP
-#ifdef PP_BSF_NUM_THREADS
-	cout << "Number of Threads: " << PP_BSF_NUM_THREADS << endl;
-#else
-	cout << "Number of Threads: " << omp_get_num_procs() << endl;
-#endif // PP_BSF_NUM_THREADS
-#else
-	cout << "OpenMP is turned off!" << endl;
-#endif // PP_BSF_OMP
-
-}
-
-void PC_bsf_CopyParameter(PT_bsf_parameter_T parameterIn, PT_bsf_parameter_T* parameterOutP) {
-
-}
-
-void PC_bsf_IterOutput(PT_bsf_reduceElem_T* reduceResult, int reduceCounter, PT_bsf_parameter_T parameter,
-	double elapsedTime, int nextJob) {	// For Job 0
-	cout << "------------------ " << BSF_sv_iterCounter << " ------------------" << endl;
-
-}
-
-void PC_bsf_IterOutput_1(PT_bsf_reduceElem_T_1* reduceResult, int reduceCounter, PT_bsf_parameter_T parameter,
-	double elapsedTime, int nextJob) {	// For Job 1
-	cout << "------------------ " << BSF_sv_iterCounter << " ------------------" << endl;
-	// Optional filling. Do not delete!
-
-}
-
-void PC_bsf_IterOutput_2(PT_bsf_reduceElem_T_2* reduceResult, int reduceCounter, PT_bsf_parameter_T parameter,
-	double elapsedTime, int nextJob) {	// For Job 2
-	cout << "------------------ " << BSF_sv_iterCounter << " ------------------" << endl;
-	// Optional filling. Do not delete!
-
-}
-
-void PC_bsf_IterOutput_3(PT_bsf_reduceElem_T_3* reduceResult, int reduceCounter, PT_bsf_parameter_T parameter,
-	double elapsedTime, int nextJob) {	// For Job 3
-	cout << "------------------ " << BSF_sv_iterCounter << " ------------------" << endl;
-	// Optional filling. Do not delete!
-
-}
-
-void PC_bsf_ProblemOutput(PT_bsf_reduceElem_T* reduceResult, int reduceCounter, PT_bsf_parameter_T parameter,
-	double t) {	// For Job 0
-
-}
-
-void PC_bsf_ProblemOutput_1(PT_bsf_reduceElem_T_1* reduceResult, int reduceCounter, PT_bsf_parameter_T parameter,
-	double t) {	// For Job 1
+void PC_bsf_ReduceF_2(PT_bsf_reduceElem_T_2* x, PT_bsf_reduceElem_T_2* y, PT_bsf_reduceElem_T_2* z) {	// For Job 2
 	// Optional filling. Do not delete!
 }
 
-void PC_bsf_ProblemOutput_2(PT_bsf_reduceElem_T_2* reduceResult, int reduceCounter, PT_bsf_parameter_T parameter,
-	double t) {	// For Job 2
-	// Optional filling. Do not delete!
+void PC_bsf_ReduceF_3(PT_bsf_reduceElem_T_3* x, PT_bsf_reduceElem_T_3* y, PT_bsf_reduceElem_T_3* z) {	// For Job 3
+	// optional filling
 }
 
-void PC_bsf_ProblemOutput_3(PT_bsf_reduceElem_T_3* reduceResult, int reduceCounter, PT_bsf_parameter_T parameter,
-	double t) {	// For Job 3
-	// Optional filling. Do not delete!
+void PC_bsf_SetInitParameter(PT_bsf_parameter_T* parameter) {
+
+}
+
+void PC_bsf_SetListSize(int* listSize) {
+
+}
+
+void PC_bsf_SetMapListElem(PT_bsf_mapElem_T* elem, int i) {
+
 }
 
 //----------------------- Assigning Values to BSF-skeleton Variables (Do not modify!) -----------------------
